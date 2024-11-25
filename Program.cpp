c6a659e408998464f7ce84c00cfc81ca9e2085e9
Hassoo15 <Hassantariq7822@gmail.com>
2024-11-25 01:07:38 -0800

    Create Program.cpp
    my first program

---
+++ Program.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2, num3;
+    float max;
+
+    printf("Enter three numbers: ");
+    scanf("%f %f %f", &num1, &num2, &num3);
+
+    max = num1;
+    if (num2 > max) {
+        max = num2;
+    }
+    if (num3 > max) {
+        max = num3;
+    }
+
+    printf("The maximum of the three numbers is: %.2f\n", max);
+
+    return 0;
+}
